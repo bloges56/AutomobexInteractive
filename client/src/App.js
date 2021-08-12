a61567e868b356435dd98fc2f7f492bfe09dfe3c
bloges56 <brady.drew.logan@gmail.com>
2021-08-12 07:53:43 -0500

    implemented simple login

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Login } from "./components/Login"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route render={() => {
+            if (localStorage.getItem("user_id")) {
+                return (
+                    <>
+                    <ApplicationViews />  
+                    </>
+                )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Router>
   );
 }
 
